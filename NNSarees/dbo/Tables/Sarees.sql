afab13e23f5685ef0077121ec9013ef4e978e5cc
nagis <nagisetty1@gmail.com>
2025-07-27 06:48:37 +0530

    NNSarees SQL Server Database project added

---
+++ NNSarees/dbo/Tables/Sarees.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Sarees] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX)  NULL,
+    [Description] NVARCHAR (MAX)  NULL,
+    [ImageUrl]    NVARCHAR (MAX)  NULL,
+    [Price]       DECIMAL (18, 2) NOT NULL,
+    [Category]    NVARCHAR (MAX)  NULL,
+    [CreatedAt]   DATETIME2 (7)   NOT NULL,
+    [UpdatedAt]   DATETIME2 (7)   NOT NULL,
+    CONSTRAINT [PK_Sarees] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
